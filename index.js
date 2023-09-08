b964a393e6f835d8e14e1a7fbb6dffee9a8d12ee
SUJITH T S <tssujith2002@gmail.com>
2023-09-08 13:00:01 +0530

    basic calculator

---
+++ index.js
@@ -0,0 +1,32 @@
+const buttons = document.querySelectorAll('button');
+let screen = document.querySelector('.screen')
+
+let calculation = [];
+let expression
+
+function calculate(button) {
+    const value = button.textContent;
+    if(value === "CLEAR"){
+        calculation = [];
+        screen.textContent = '';
+    } else if(value === "="){
+        try{
+            expression = eval(expression);
+            screen.textContent = expression;
+            calculation = [expression];
+        }catch(e){
+            screen.textContent = `"Oops, input is invalid."  `
+            calculation = [];
+        }
+       
+    }else{
+        calculation.push(value);
+        console.log(calculation);
+        expression = calculation.join('');
+        screen.textContent = expression;
+    }
+}
+
+buttons.forEach(button => 
+    button.addEventListener('click',() =>   
+                                    calculate(button)));
